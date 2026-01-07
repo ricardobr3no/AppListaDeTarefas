e0036328830123b8e649fe8892d3a77eee2c42ba
AslanAD <azevedo.calebe34@gmail.com>
2026-01-07 17:40:03 -0300

    feat: adicionando status e classe tarefa ja com seus metodos

---
+++ src/Tarefa.java
@@ -0,0 +1,29 @@
+public class Tarefa {
+
+    private String nome;
+    private String descricao;
+    private Status status;
+
+    public Tarefa(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.status = Status.DISPONIVEL;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
+
