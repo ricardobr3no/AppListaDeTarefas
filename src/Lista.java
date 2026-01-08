67e2cdeeb5a89f3a6bd67334357ecf27c4c2ad1b
Ricardo Breno <kbreeno.18@gmail.com>
2026-01-08 17:05:45 -0300

    Revert "refact: refact Lista.java e ListaTarefas.java"
    This reverts commit 75e0b9b40f21f0f6c3a07437df02929f87ab2e97.

--- src/Lista.java
+++ src/Lista.java
@@ -1,45 +1,59 @@
 import java.util.ArrayList;
+import java.util.List;
 
+public class Lista {
 
-public class Lista extends ArrayList<Tarefa> {
+    private static Lista instancia;
+    private List<Tarefa> tarefas;
+
+    private Lista() {
+        tarefas = new ArrayList<>();
+    }
+
+    public static Lista getInstancia() {
+        if (instancia == null) {
+            instancia = new Lista();
+        }
+        return instancia;
+    }
 
     public void adicionarTarefa(Tarefa tarefa) {
-        this.add(tarefa);
+        tarefas.add(tarefa);
     }
 
     public void removerTarefa(int index) {
-        if (index >= 0 && index < this.size()) {
-            this.remove(index);
+        if (index >= 0 && index < tarefas.size()) {
+            tarefas.remove(index);
         } else {
             System.out.println("Índice inválido!");
         }
     }
 
     public void alterarStatus(int index, AlterarStatusStrategy strategy) {
-        if (index >= 0 && index < this.size()) {
-            strategy.alterarStatus(this.get(index));
+        if (index >= 0 && index < tarefas.size()) {
+            strategy.alterarStatus(tarefas.get(index));
         } else {
             System.out.println("Índice inválido!");
         }
     }
 
     public void listarTarefas() {
-        if (this.isEmpty()) {
+        if (tarefas.isEmpty()) {
             System.out.println("Nenhuma tarefa cadastrada.");
             return;
         }
 
-        System.out.println("============================================================================");
-        System.out.printf(" %-5s | %-20s | %-30s | %-10s \n", "INDEX", "NOME", "DESCRICAO", "STATUS");
-        System.out.println("============================================================================");
+        System.out.println("=================================================");
+        System.out.println("INDEX | NOME       | DESCRIÇÃO       | STATUS");
+        System.out.println("=================================================");
 
-        for (int idx = 0; idx < this.size(); idx++) {
-            Tarefa t = this.get(idx);
+        for (int i = 0; i < tarefas.size(); i++) {
+            Tarefa t = tarefas.get(i);
             System.out.printf(
-                    " %-5d | %-20s | %-30s | %-10s \n",
-                    idx, t.getNome(), t.getDescricao(), t.getStatus()
+                    "%-5d | %-10s | %-15s | %-10s%n",
+                    i, t.getNome(), t.getDescricao(), t.getStatus()
             );
         }
-        System.out.println("============================================================================");
+        System.out.println("=================================================");
     }
 }
--- src/ListaTarefas.java
+++ src/ListaTarefas.java
@@ -5,7 +5,7 @@ public class ListaTarefas {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Lista lista = new Lista();
+        Lista lista = Lista.getInstancia();
         int opcao;
 
         do {
